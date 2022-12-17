704a7b33ba6a328257a880a2bca668cbd637eb59
Adem KOCAMAZ <kocamazadem@gmail.com>
2022-12-17 21:50:09 +0300

    Update forge.config.js

--- dist/forge.config.js
+++ dist/forge.config.js
@@ -1,5 +1,7 @@
 module.exports = {
-  packagerConfig: {},
+  packagerConfig: {
+    asar: true,
+  },
   rebuildConfig: {},
   makers: [
     {
@@ -8,7 +10,6 @@ module.exports = {
     },
     {
       name: '@electron-forge/maker-zip',
-      platforms: ['darwin'],
     },
     {
       name: '@electron-forge/maker-deb',
